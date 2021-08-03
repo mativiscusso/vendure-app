3239af44f5c691bdefda449994064470785a528e
mativiscusso <mativiscusso@gmail.com>
2021-08-03 15:08:02 -0300

    third commit

--- src/vendure-config.js
+++ src/vendure-config.js
@@ -1,90 +1,13 @@
 const {
     dummyPaymentHandler,
-    LanguageCode,
     DefaultJobQueuePlugin,
     DefaultSearchPlugin,
-    PaymentMethodHandler,
 } = require("@vendure/core");
 const { defaultEmailHandlers, EmailPlugin } = require("@vendure/email-plugin");
 const { AssetServerPlugin } = require("@vendure/asset-server-plugin");
 const { AdminUiPlugin } = require("@vendure/admin-ui-plugin");
-
 const path = require("path");
-require("dotenv").config();
-
-const mercadopago = require("mercadopago");
-
-const mercadoPagoIntegration = new PaymentMethodHandler({
-    code: "mercado-pago-handler",
-    description: [
-        {
-            languageCode: LanguageCode.es,
-            value: "Mercado Pago",
-        },
-    ],
-    args: {
-        apiKey: { type: "string" },
-    },
-
-    /** This is called when the `addPaymentToOrder` mutation is executed */
-    createPayment: async (ctx, order, amount, args, metadata) => {
-        try {
-            mercadopago.configurations.setAccessToken(
-                "TEST-4314139111445738-071414-8595ac6daacb5c4d18e1ed4e8058704e-191921434"
-            );
-            const payment_data = {
-                transaction_amount: order.total,
-                token: metadata.token,
-                description: metadata.description,
-                installments: Number(metadata.installments),
-                payment_method_id: metadata.paymentMethodId,
-                issuer_id: metadata.issuer_id,
-                payer: {
-                    email: metadata.payer.email,
-                    identification: {
-                        type: metadata.payer.identification.type,
-                        number: metadata.payer.identification.number,
-                    },
-                },
-            };
-
-            console.log(payment_data, metadata);
-            const resultPayment = await mercadopago.payment.save(payment_data);
-
-            return {
-                amount: order.total,
-                state: "Settled",
-                transactionId: resultPayment.body.id.toString(),
-                metadata: {
-                    public: {
-                        ...resultPayment.body,
-                    },
-                },
-            };
-        } catch (err) {
-            console.log(err);
-            return {
-                amount: order.total,
-                state: "Declined",
-                metadata: {
-                    errorMessage: err.message,
-                },
-            };
-        }
-    },
-
-    /** This is called when the `settlePayment` mutation is executed */
-    settlePayment: async (ctx, order, payment, args) => {
-        try {
-            return { success: true };
-        } catch (err) {
-            return {
-                success: false,
-                errorMessage: err.message,
-            };
-        }
-    },
-});
+require('dotenv').config()
 
 const config = {
     apiOptions: {
@@ -109,9 +32,9 @@ const config = {
             identifier: "superadmin",
             password: "superadmin",
         },
-        tokenMethod: "cookie",
+        tokenMethod: 'cookie',
         cookieOptions: {
-            secret: process.env.COOKIE_SESSION_SECRET,
+          secret: process.env.COOKIE_SESSION_SECRET
         },
         requireVerification: true,
     },
@@ -127,7 +50,7 @@ const config = {
         migrations: [path.join(__dirname, "../migrations/*.ts")],
     },
     paymentOptions: {
-        paymentMethodHandlers: [mercadoPagoIntegration, dummyPaymentHandler],
+        paymentMethodHandlers: [dummyPaymentHandler],
     },
     customFields: {},
     plugins: [
