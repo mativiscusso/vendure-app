67feb669e0cb3f73e576082baa9f39ccd2f475d5
mativiscusso <mativiscusso@gmail.com>
2021-08-04 14:47:37 -0300

    Revert "upgrade vendure"
    This reverts commit 527a51cc2577337d42f8ee2b9fe7299dfcd0a412.

--- src/vendure-config.js
+++ src/vendure-config.js
@@ -7,40 +7,46 @@ const { defaultEmailHandlers, EmailPlugin } = require("@vendure/email-plugin");
 const { AssetServerPlugin } = require("@vendure/asset-server-plugin");
 const { AdminUiPlugin } = require("@vendure/admin-ui-plugin");
 const path = require("path");
+require("dotenv").config();
 
 const config = {
     apiOptions: {
-        port: process.env.PORT,
+        port: 8080,
         adminApiPath: "admin-api",
         adminApiPlayground: {
             settings: {
                 "request.credentials": "include",
             },
         }, // turn this off for production
-        adminApiDebug: process.env.TURN_ON_API_OPTIONS, // turn this off for production
+        adminApiDebug: true, // turn this off for production
         shopApiPath: "shop-api",
         shopApiPlayground: {
             settings: {
                 "request.credentials": "include",
             },
         }, // turn this off for production
-        shopApiDebug: process.env.TURN_ON_API_OPTIONS, // turn this off for production
+        shopApiDebug: true, // turn this off for production
     },
     authOptions: {
         superadminCredentials: {
-            identifier: "superadmin",
-            password: "superadmin",
+            identifier: process.env.SUPERADMIN_USERNAME,
+            password: process.env.SUPERADMIN_PASSWORD,
         },
+        tokenMethod: "cookie",
+        cookieOptions: {
+            secret: process.env.COOKIE_SESSION_SECRET,
+        },
+        requireVerification: true,
     },
     dbConnectionOptions: {
         type: "mysql",
-        synchronize: process.env.TURN_ON_API_OPTIONS, // turn this off for production
+        synchronize: true, // turn this off for production
         logging: false,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         username: process.env.DB_USER,
-        password: process.env.DB_PASS,
+        password: process.env.DB_PASSWORD,
         migrations: [path.join(__dirname, "../migrations/*.ts")],
     },
     paymentOptions: {
@@ -63,9 +69,9 @@ const config = {
             globalTemplateVars: {
                 // The following variables will change depending on your storefront implementation
                 fromAddress: '"example" <noreply@example.com>',
-                verifyEmailAddressUrl: `${process.env.DOMAIN_URL}/verify`,
-                passwordResetUrl: `${process.env.DOMAIN_URL}/password-reset`,
-                changeEmailAddressUrl: `${process.env.DOMAIN_URL}/verify-email-address-change`,
+                verifyEmailAddressUrl: `${process.env.DOMAIN_URL}/users/verify`,
+                passwordResetUrl: `${process.env.DOMAIN_URL}/users/password-reset`,
+                changeEmailAddressUrl: `${process.env.DOMAIN_URL}/users/verify-email-address-change`,
             },
         }),
         AdminUiPlugin.init({
